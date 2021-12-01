ce053795ee8e37cecfd1d5ae5dd4222bf4423220
Danucas <danrodcastillo1994@gmail.com>
2021-11-30 22:02:57 -0500

    utils change for printing

--- api/v1/utils.py
+++ api/v1/utils.py
@@ -5,13 +5,13 @@ import json
 def save_status(device_id, st, code=None, data=None):
     stats = get_status(device_id)
     stats['code'] = code
-    stats['st'] = st
+    stats['action'] = st
     if data:
         stats['data'] = data
-
     status_path = f'{os.getcwd()}/sessions/{device_id}.status'
     with open(status_path, 'w') as status_file:
         status_file.write(json.dumps(stats))
+    print(f'Status saved in {status_path}')
 
 
 def get_status(device_id):
